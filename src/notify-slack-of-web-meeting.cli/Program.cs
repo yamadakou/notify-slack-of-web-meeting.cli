f64bde5cadfe396b11c2a76cc3af0b78a5b7e17c
yamada koji <koji.yamada.j7k@jpgr.denso.com>
2022-03-02 23:08:42 +0900

    R HTTPクライアントをリトライ可能なHTTP要求に対応

--- src/notify-slack-of-web-meeting.cli/Program.cs
+++ src/notify-slack-of-web-meeting.cli/Program.cs
@@ -14,6 +14,29 @@ using notify_slack_of_web_meeting.cli.SlackChannels;
 using notify_slack_of_web_meeting.cli.WebMeetings;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 using Outlook = Microsoft.Office.Interop.Outlook;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Polly;
+using Polly.Extensions.Http;
+
+// # ※リトライ可能なHTTP要求を実現するための参考
+// ## Microsoft Docs
+// * IHttpClientFactory を使用して回復力の高い HTTP 要求を実装する
+//   * https://docs.microsoft.com/ja-jp/dotnet/architecture/microservices/implement-resilient-applications/use-httpclientfactory-to-implement-resilient-http-requests
+// * IHttpClientFactory ポリシーと Polly ポリシーで指数バックオフを含む HTTP 呼び出しの再試行を実装する
+//   * https://docs.microsoft.com/ja-jp/dotnet/architecture/microservices/implement-resilient-applications/implement-http-call-retries-exponential-backoff-polly
+// ## Blog
+// * C# - HttpClientFactoryをDIの外で使う
+//   * https://dekirukigasuru.com/blog/2020/04/24/csharp-httpclientfactory/
+// * C# - HttpClientFactoryとPollyで回復力の高い何某
+//   * https://dekirukigasuru.com/blog/2020/05/15/csharp-httpclientfactory-polly/
+// ## Nuget
+// * Microsoft.Extensions.DependencyInjection
+//   * https://www.nuget.org/packages/Microsoft.Extensions.DependencyInjection/5.0.2
+// * Microsoft.Extensions.Http
+//   * https://www.nuget.org/packages/Microsoft.Extensions.Http/5.0.0
+// * Microsoft.Extensions.Http.Polly
+//   * https://www.nuget.org/packages/Microsoft.Extensions.Http.Polly/5.0.1
 
 namespace notify_slack_of_web_meeting.cli
 {
@@ -22,7 +45,7 @@ namespace notify_slack_of_web_meeting.cli
         /// <summary>
         /// HTTPクライアント
         /// </summary>
-        private static HttpClient s_HttpClient = new HttpClient();
+        private static HttpClient s_HttpClient;
 
         [Verb("setting", HelpText = "Register Slack channel information and create a configuration file.")]
         public class SettingOptions
@@ -52,6 +75,21 @@ namespace notify_slack_of_web_meeting.cli
 
         static int Main(string[] args)
         {
+            // Serviceにリトライ可能なHTTPクライアントを設定
+            var services = new ServiceCollection();
+            services.AddHttpClient("RetryHttpClient")
+            .SetHandlerLifetime(TimeSpan.FromMinutes(5))  // ライフタイムを5分に設定
+            .AddPolicyHandler(GetRetryPolicy());
+
+            // Microsoft DependancyInjection
+            var provider = services.BuildServiceProvider();
+
+            // Microsoft Http クライアントファクトリーを取得
+            var factory = provider.GetRequiredService<IHttpClientFactory>();
+
+            // リトライ可能なHTTPクライアントを取得
+            s_HttpClient = factory.CreateClient("RetryHttpClient");
+
             // Settingコマンドを定義
             Func<SettingOptions, int> RunSettingAndReturnExitCode = opts =>
             {
@@ -257,5 +295,27 @@ namespace notify_slack_of_web_meeting.cli
             }
             catch { return null; }
         }
+
+        /// <summary>
+        /// リトライのポリシーを取得する
+        /// </summary>
+        /// <returns>リトライのポリシー</returns>
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            // 指数関数的再試行で (最初は 2 秒) 6 回試すポリシー
+            // リトライ対象となる条件は以下
+            // * Network failures (as HttpRequestException)
+            // * HTTP 5XX status codes (server errors)
+            // * HTTP 408 status code (request timeout)
+            // * HTTP 429 status code (too many requests)
+            // ※参考
+            // https://github.com/App-vNext/Polly/wiki/Polly-and-HttpClientFactory#using-addtransienthttperrorpolicy
+            // https://github.com/App-vNext/Polly.Extensions.Http/blob/master/src/Polly.Extensions.Http/HttpPolicyExtensions.cs#L17-L28
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+                .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+
     }
 }
